d5203ae1637a708b3c7e65cfd96cddbda4168fa5
thimira nirmal <timnirmal@gmail.com>
2021-11-21 14:49:39 +0530

    hotfix : Replace Barcode reader with first Code

--- app/components/Tasks.js
+++ app/components/Tasks.js
@@ -1,40 +1,105 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
+const Tasks = () => {
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  // const [text, setText] = useState('Not yet scanned')
 
+  const askForCameraPermission = () => {
+    (async () => {
+      // const status = await Permissions.askAsync(Permissions.CAMERA);
+      console.log('permission status'+status);
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })()
+  }
+
+  // Request Camera Permission
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data)
+    console.log('Type: ' + type + '\nData: ' + data)
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+        <View style={styles.container}>
+          <Text>Requesting for camera permission</Text>
+        </View>)
+  }
+  if (hasPermission === false) {
+    return (
+        <View style={styles.container}>
+          <Text style={{ margin: 10 }}>No access to camera</Text>
+          <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+        </View>)
+  }
+
+  // Return the View
+  return (
+      <View style={styles.container}>
+        <View style={styles.barcodebox}>
+          <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+              style={{ height: 400, width: 400 }} />
+        </View>
+        <Text style={styles.maintext}>{text}</Text>
+
+        {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+      </View>
+  );
+}
+/*
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+const Tasks = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
   useEffect(() => {
     (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
   }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
   if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+    return <View />;
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
   return (
-      <View style={styles.container}>
-        <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
-        />
-        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-      </View>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
 }
+// const styles = StyleSheet.create({ ... });
+*/
 
 const styles = StyleSheet.create({
   container: {
@@ -57,3 +122,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'tomato'
   }
 });
+
+export default Tasks;
